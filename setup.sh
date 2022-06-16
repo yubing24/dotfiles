815bcbea841870e14448e2941971f81f655f7e75
Yubing Hou <houyubing24@gmail.com>
2022-06-16 01:25:55 -0500

    consolidated bash configuration; stablized emacs configuration

--- setup.sh
+++ setup.sh
@@ -6,11 +6,14 @@ ln -nfs $(pwd)/emacs.d/ $HOME/.emacs.d
 # configure path to necesary files
 mkdir -p $(pwd)/emacs.d/var/auto-save/
 
+# Bash Setup
+ln -nfs $(pwd)/bashrc $HOME/.bashrc
+
 # Git setup
 ln -nfs $(pwd)/git/gitconfig $HOME/.gitconfig
 
-# Profile
-ln -nfs $(pwd)/profile $HOME/.profile
-
 # Ag
 ln -nfs $(pwd)/agignore $HOME/.agignore
+
+# NPM RC
+ln -nfs $(pwd)/npmrc $HOME/.npmrc
